4fa5fee35b16208899f97d9793d2e1942462fc4e
Yassa-hue <yassatamer99@gmail.com>
2025-04-18 12:15:25 +0200

    Use rayon to make it parallel

--- src/board_manager.rs
+++ src/board_manager.rs
@@ -5,6 +5,7 @@ use crate::pieces::types::move_direction::{
   SpecialMove, SpecialMoveValidationAction,
 };
 use crate::pieces::types::position::Position;
+use rayon::prelude::*;
 use std::collections::HashMap;
 
 pub struct BoardManager {
@@ -46,7 +47,7 @@ impl BoardManager {
   }
 
   fn can_apply_move(
-    &mut self,
+    &self,
     piece_position: Position,
     target_position: Position,
     current_player_color: Color,
@@ -84,7 +85,7 @@ impl BoardManager {
   }
 
   fn validate_move_basics(
-    &mut self,
+    &self,
     piece_position: Position,
     current_player_color: Color,
   ) -> Result<(), String> {
@@ -122,7 +123,7 @@ impl BoardManager {
   }
 
   fn validate_special_move(
-    &mut self,
+    &self,
     action: SpecialMoveValidationAction,
     from: Position,
     to: Position,
@@ -132,7 +133,7 @@ impl BoardManager {
   }
 
   fn validate_piece_exists(
-    &mut self,
+    &self,
     piece_position: Position,
   ) -> Result<(), String> {
     if self.chessboard.is_position_empty(piece_position) {
@@ -143,7 +144,7 @@ impl BoardManager {
   }
 
   fn validate_player_owns_piece(
-    &mut self,
+    &self,
     piece_position: Position,
     current_player_color: Color,
   ) -> Result<(), String> {
@@ -154,32 +155,33 @@ impl BoardManager {
     Ok(())
   }
 
-  fn is_king_checked(&mut self, current_player_color: Color) -> bool {
+  fn is_king_checked(&self, current_player_color: Color) -> bool {
     let enemy_color = current_player_color.next();
-    let king_position = self.chessboard.get_king_position(enemy_color);
+    let king_position = match self.chessboard.get_king_position(enemy_color) {
+      Some(pos) => pos,
+      None => return false,
+    };
 
-    if king_position.is_none() {
-      return false;
-    }
-
-    let king_position = king_position.unwrap();
-
-    for position in self.chessboard.get_all_positions() {
-      if self.chessboard.is_position_empty(position) {
-        continue;
-      }
-      let piece = self.chessboard.get_piece(position).unwrap();
-
-      if piece.is_of_color(current_player_color) {
-        if self
-          .can_apply_move(position, king_position, current_player_color)
-          .is_ok()
-        {
-          return true;
+    self
+      .chessboard
+      .get_all_positions()
+      .par_iter()
+      .filter_map(|&position| {
+        if self.chessboard.is_position_empty(position) {
+          return None;
         }
-      }
-    }
-    false
+        let piece = self.chessboard.get_piece(position)?;
+        if piece.is_of_color(current_player_color) {
+          Some(position)
+        } else {
+          None
+        }
+      })
+      .any(|from| {
+        self
+          .can_apply_move(from, king_position, current_player_color)
+          .is_ok()
+      })
   }
 
   pub fn upgrade_piece(
@@ -217,11 +219,11 @@ impl BoardManager {
   fn get_special_move_validation_action(
     &self,
     special_move_validation: SpecialMoveValidationAction,
-  ) -> Box<dyn Fn(&mut BoardManager, Position, Position) -> bool> {
+  ) -> Box<dyn Fn(&BoardManager, Position, Position) -> bool> {
     let mut special_move_validation_functions = HashMap::new();
     special_move_validation_functions.insert(
       SpecialMoveValidationAction::EnemyPieceExists,
-      |board_manager: &mut BoardManager,
+      |board_manager: &BoardManager,
        piece_position: Position,
        target_position: Position| {
         if board_manager.chessboard.is_position_empty(target_position) {
